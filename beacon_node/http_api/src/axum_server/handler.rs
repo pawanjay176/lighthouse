9fc65cdbbccc11b15f0885e2da14ab4e5b781bf2
Pawan Dhananjay <pawandhananjay@gmail.com>
2024-01-29 19:05:04 +0530

    Tackle multi queries

--- beacon_node/http_api/src/axum_server/handler.rs
+++ beacon_node/http_api/src/axum_server/handler.rs
@@ -1,14 +1,10 @@
-use axum::{
-    debug_handler,
-    extract::State,
-    extract::{Path, Query},
-    Error, Extension, Json,
-};
-use std::{str::FromStr, sync::Arc};
+use axum::extract::RawQuery;
+use axum::{debug_handler, extract::Path, extract::State, Error, Extension, Json};
+use std::{collections::HashMap, str::FromStr, sync::Arc};
 
-use crate::state_id::StateId;
+use crate::{state_id::StateId, validator};
 use beacon_chain::{BeaconChain, BeaconChainTypes};
-use eth2::types as api_types;
+use eth2::types::{self as api_types, ValidatorBalanceData, ValidatorBalancesQuery, ValidatorId};
 use eth2::types::{ExecutionOptimisticFinalizedResponse, GenericResponse, GenesisData, RootData};
 
 use super::error::Error as HandlerError;
@@ -28,6 +24,11 @@ fn chain_filter<T: BeaconChainTypes>(
     }
 }
 
+pub async fn catch_all() -> &'static str {
+    dbg!("yaha aaya");
+    "whoaaa"
+}
+
 /// GET beacon/genesis
 pub async fn get_beacon_genesis<T: BeaconChainTypes>(
     State(chain_state): State<Arc<ChainState<T>>>,
@@ -42,7 +43,6 @@ pub async fn get_beacon_genesis<T: BeaconChainTypes>(
 }
 
 /// GET beacon/states/{state_id}/root
-// #[debug_handler]
 pub async fn get_beacon_state_root<T: BeaconChainTypes>(
     State(chain_state): State<Arc<ChainState<T>>>,
     Path(state_id): Path<String>,
@@ -53,4 +53,80 @@ pub async fn get_beacon_state_root<T: BeaconChainTypes>(
     Ok(GenericResponse::from(api_types::RootData::from(root)))
         .map(|resp| resp.add_execution_optimistic_finalized(execution_optimistic, finalized))
         .map(Json)
+}
+
+/// GET beacon/states/{state_id}/fork
+pub async fn get_beacon_state_fork<T: BeaconChainTypes>(
+    State(chain_state): State<Arc<ChainState<T>>>,
+    Path(state_id): Path<String>,
+) -> Result<Json<ExecutionOptimisticFinalizedResponse<api_types::Fork>>, HandlerError> {
+    let chain = chain_filter(chain_state)?;
+    let state_id = StateId::from_str(&state_id)?;
+    let (fork, execution_optimistic, finalized) =
+        state_id.fork_and_execution_optimistic_and_finalized(&chain)?;
+    Ok(GenericResponse::from(api_types::Fork::from(fork)))
+        .map(|resp| resp.add_execution_optimistic_finalized(execution_optimistic, finalized))
+        .map(Json)
+}
+
+/// GET beacon/states/{state_id}/finality_checkpoints
+pub async fn get_beacon_state_finality_checkpoints<T: BeaconChainTypes>(
+    State(chain_state): State<Arc<ChainState<T>>>,
+    Path(state_id): Path<String>,
+) -> Result<
+    Json<ExecutionOptimisticFinalizedResponse<api_types::FinalityCheckpointsData>>,
+    HandlerError,
+> {
+    let chain = chain_filter(chain_state)?;
+    let state_id = StateId::from_str(&state_id)?;
+    let (data, execution_optimistic, finalized) = state_id
+        .map_state_and_execution_optimistic_and_finalized(
+            &chain,
+            |state, execution_optimistic, finalized| {
+                Ok((
+                    api_types::FinalityCheckpointsData {
+                        previous_justified: state.previous_justified_checkpoint(),
+                        current_justified: state.current_justified_checkpoint(),
+                        finalized: state.finalized_checkpoint(),
+                    },
+                    execution_optimistic,
+                    finalized,
+                ))
+            },
+        )?;
+    Ok(api_types::ExecutionOptimisticFinalizedResponse {
+        data,
+        execution_optimistic: Some(execution_optimistic),
+        finalized: Some(finalized),
+    })
+    .map(Json)
+}
+
+/// GET beacon/states/{state_id}/validator_balances?id
+pub async fn get_beacon_state_validator_balances<T: BeaconChainTypes>(
+    State(chain_state): State<Arc<ChainState<T>>>,
+    Path(state_id): Path<String>,
+    RawQuery(query): RawQuery, // Should probably have a cleaner solution for this
+) -> Result<Json<ExecutionOptimisticFinalizedResponse<Vec<ValidatorBalanceData>>>, HandlerError> {
+    let chain = chain_filter(chain_state)?;
+    let state_id = StateId::from_str(&state_id)?;
+    let validator_queries = if let Some(query_str) = query {
+        let validator_queies: ValidatorBalancesQuery = serde_array_query::from_str(&query_str)
+            .map_err(|e| {
+                HandlerError::Other(format!(
+                    "Failed to parse query string: Query string: {} error: {:?}",
+                    query_str, e
+                ))
+            })?;
+        validator_queies.id
+    } else {
+        None
+    };
+    crate::validators::get_beacon_state_validator_balances(
+        state_id,
+        chain,
+        validator_queries.as_deref(),
+    )
+    .map_err(HandlerError::Warp)
+    .map(Json)
 }
--- beacon_node/http_api/src/axum_server/mod.rs
+++ beacon_node/http_api/src/axum_server/mod.rs
@@ -18,7 +18,7 @@ use std::sync::Arc;
 use std::future::{Future, IntoFuture};
 use std::net::{SocketAddr, TcpListener};
 use tower::ServiceBuilder;
-use tower_http::trace::{TraceLayer, DefaultOnRequest};
+use tower_http::trace::{DefaultOnRequest, TraceLayer};
 
 pub async fn serve<T: BeaconChainTypes>(
     ctx: Arc<Context<T>>,
@@ -47,6 +47,18 @@ pub fn routes<T: BeaconChainTypes>(chain_state: Arc<ChainState<T>>) -> Router {
         .route(
             "/beacon/states/:state_id/root",
             get(handler::get_beacon_state_root::<T>),
+        )
+        .route(
+            "/beacon/states/:state_id/fork",
+            get(handler::get_beacon_state_fork::<T>),
+        )
+        .route(
+            "/beacon/states/:state_id/finality_checkpoints",
+            get(handler::get_beacon_state_finality_checkpoints::<T>),
+        )
+        .route(
+            "/beacon/states/:state_id/validator_balances",
+            get(handler::get_beacon_state_validator_balances::<T>),
         )
         .fallback(handler::catch_all)
         // .layer(tower_http::trace::TraceLayer::new_for_http())
@@ -91,14 +103,14 @@ mod tests {
         extract::connect_info::MockConnectInfo,
         http::{self, Request, StatusCode},
     };
+    use http_body_util::BodyExt;
     use serde_json::{json, Value};
-    use std::net::SocketAddr;
+    use std::{collections::HashMap, net::SocketAddr};
     use tokio::net::TcpListener;
     use tower::{Service, ServiceExt}; // for `call`, `oneshot`, and `ready`
-    use types::{EthSpec, MainnetEthSpec};
-    use http_body_util::BodyExt; 
     use tracing::{info_span, Span};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+    use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+    use types::{EthSpec, MainnetEthSpec};
 
     use super::super::test_utils::InteractiveTester;
 
@@ -107,15 +119,16 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_axum_genesis() {
         tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-                // axum logs rejections from built-in extractors with the `axum::rejection`
-                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
-                "example_tracing_aka_logging=debug,tower_http=debug,axum::rejection=trace".into()
-            }),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
+            .with(
+                tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                    // axum logs rejections from built-in extractors with the `axum::rejection`
+                    // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
+                    "example_tracing_aka_logging=debug,tower_http=debug,axum::rejection=trace"
+                        .into()
+                }),
+            )
+            .with(tracing_subscriber::fmt::layer())
+            .init();
         let validator_count = 24;
         let spec = E::default_spec();
 
@@ -127,7 +140,7 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
         let response = app
             .oneshot(
                 Request::builder()
-                    .uri("/beacon/states/finalized/root")
+                    .uri("/beacon/states/finalized/validator_balances?id=0,1")
                     .body(Body::empty())
                     .unwrap(),
             )
@@ -136,5 +149,24 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
         let body = response.into_body().collect().await.unwrap().to_bytes();
         let body: Value = serde_json::from_slice(&body).unwrap();
         dbg!(body);
+    }
+
+    #[test]
+    fn test_query_params() {
+        use axum::extract::Query;
+        use eth2::types::ValidatorBalancesQuery;
+        use http::Uri;
+        use serde::Deserialize;
+        use std::str::FromStr;
+
+        #[derive(Deserialize, Default)]
+        struct Temp {
+            #[serde(default)]
+            id: Vec<u64>
+        }
+
+        let uri: Uri = "http://example.com/path/?id=0,1".parse().unwrap();
+        let result: Query<Vec<(String, String)>> = Query::try_from_uri(&uri).unwrap();
+        dbg!(&result);
     }
 }
